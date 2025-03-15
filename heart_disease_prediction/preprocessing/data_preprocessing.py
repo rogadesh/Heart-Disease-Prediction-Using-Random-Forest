f2cdf678782f3fabec684ece42b555002a6877ca
rogadesh <adesh11501@gmail.com>
2025-03-15 22:48:14 +0530

    Delete heart_disease_prediction directory

--- heart_disease_prediction/preprocessing/data_preprocessing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-
-def preprocess_data(df):
-    df = df.copy()
-    scaler = StandardScaler()
-    df_scaled = scaler.fit_transform(df)
-    return df_scaled, scaler
